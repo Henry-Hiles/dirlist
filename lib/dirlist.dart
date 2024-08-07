66e8f5c73308a7302f6e74b4434d2a8adc695b80
Henry-Hiles <henry@henryhiles.com>
2024-08-07 12:09:17 -0400

    Try to fix condition

--- lib/dirlist.dart
+++ lib/dirlist.dart
@@ -2,17 +2,30 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
 
-/// Generates a single-line comment for each class
 class DirlistGenerator extends GeneratorForAnnotation<Dirlist> {
   const DirlistGenerator();
 
   @override
   Future<String> generateForAnnotatedElement(
-      Element element, ConstantReader annotation, BuildStep buildStep) async {
-    final library = LibraryReader(await buildStep.inputLibrary);
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) async {
+    // final library = LibraryReader();
     final extendedClass = annotation.read("extendedClass").typeValue;
+    print((await buildStep.inputLibrary)
+        .topLevelElements
+        .map((e) => e.children.map((e) => e.children.map((e) => e.name))));
+    final classes = []; // library.classes.where((classElement) {
+    //   print("supertype: ${classElement.supertype}");
+    //   print("class: $classElement");
 
-    return "final ${element.displayName}=[${library.classes.where((classElement) => classElement.thisType.extensionTypeErasure == extendedClass).join(",")}];";
+    //   return TypeChecker.fromStatic(
+    //     classElement.supertype!.extensionTypeErasure,
+    //   ).isAssignableFromType(extendedClass);
+    // });
+
+    return "final ${element.displayName}=[${classes.join(",")}];";
   }
 
   static Builder builder(BuilderOptions options) =>
